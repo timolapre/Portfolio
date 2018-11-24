a21aec144e721a2de64e454acdebd5a9abca0fc8
Timo Lapre <timolapre1998@gmail.com>
2018-11-24 14:32:06 +0100

    big update

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+//if(isset($_POST['submit'])){
+    $to = "timo@lapre.nl"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+    //}
 ?>
 
 <!DOCTYPE html>
--- code.js
+++ code.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     });
 
     $(".pagebut").click(function(){
-        window.location.href = '../pages/page.html' + '#'+ $(this).attr('id');
+        window.location.href = '../pages/allprojects.html' + '#'+ $(this).attr('id');
         //location.reload();
         //window.open("../pages/page" + $(this).attr('id') + ".html","_self");
     });
@@ -30,12 +30,16 @@ var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
     modal.style.display = "block";
 }
 
